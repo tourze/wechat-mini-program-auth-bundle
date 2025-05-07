a2dfe70821782f1a0e0a036b5932196eb4bddf9c
github-ci <you@example.com>
2025-05-07 14:22:43 +0000

    Clean menu provider

--- src/AdminMenu.php
+++ src/AdminMenu.php
@@ -3,13 +3,12 @@
 namespace WechatMiniProgramAuthBundle;
 
 use Knp\Menu\ItemInterface;
-use Tourze\EasyAdminMenuBundle\Attribute\MenuProvider;
 use Tourze\EasyAdminMenuBundle\Service\LinkGeneratorInterface;
+use Tourze\EasyAdminMenuBundle\Service\MenuProviderInterface;
 use WechatMiniProgramAuthBundle\Entity\CodeSessionLog;
 use WechatMiniProgramAuthBundle\Entity\User;
 
-#[MenuProvider]
-class AdminMenu
+class AdminMenu implements MenuProviderInterface
 {
     public function __construct(private readonly LinkGeneratorInterface $linkGenerator)
     {
